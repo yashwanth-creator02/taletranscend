7dca76120e9cb08d969afc67ab0253101c2d1696
yashwanth-creator02 <yashwanthsimha11@gmail.com>
2026-01-09 22:08:16 +0530

    renamed

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
-import firebaseConfig from "../firebase-config.js";
+import firebaseConfig from "../config/firebase-config.js";
 
 export const appId = "taletranscend-pro";
 
