d42c63b6af9bf57ca2f671a05046e7e869de0932
yashwanth-creator02 <yashwanthsimha11@gmail.com>
2026-07-21 17:46:28 +0530

    fixed package issue of postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/postcss';
 import autoprefixer from 'autoprefixer';
 
 export default {
-  plugins: [tailwindcss(), autoprefixer],
+  plugins: [tailwindcss, autoprefixer],
 };
